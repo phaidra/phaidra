f73af7670a71596bc3a2233cc6ce50b6591fcc5a
Rasta <hudak.rastislav@gmail.com>
2023-06-01 16:42:28 +0200

    nuxt config fix

--- components/phaidra-ui/nuxt.config.js
+++ components/phaidra-ui/nuxt.config.js
@@ -21,9 +21,9 @@ export default {
   // },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/css/d3NetworkCustom.css'
-  ],
+  // css: [
+  //   '~/assets/css/d3NetworkCustom.css'
+  // ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
