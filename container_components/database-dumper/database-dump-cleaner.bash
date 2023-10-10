bf5db35e8a0ad8470fae5610fb86188b42276a8c
Daniel Herzig <daniel.herzig@univie.ac.at>
2023-10-10 12:01:34 +0200

    add db-dumper to ssl and shib, set dumps daily, retention 30 days.

--- container_components/database-dumper/database-dump-cleaner.bash
+++ container_components/database-dumper/database-dump-cleaner.bash
@@ -1,3 +1,3 @@
-find /mnt/database-dumps -type f -name "*${PHAIDRADB}*" | sort -r | tail -n+8 | xargs rm
-find /mnt/database-dumps -type f -name "*${FEDORADB}*" | sort -r | tail -n+8 | xargs rm
-find /mnt/database-dumps -type f -name "*mongodb*" | sort -r | tail -n+8 | xargs rm
+find /mnt/database-dumps -type f -name "*${PHAIDRADB}*" | sort -r | tail -n+30 | xargs rm
+find /mnt/database-dumps -type f -name "*${FEDORADB}*" | sort -r | tail -n+30 | xargs rm
+find /mnt/database-dumps -type f -name "*mongodb*" | sort -r | tail -n+30 | xargs rm
