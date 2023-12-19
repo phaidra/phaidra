aaa987dccca7e6f78ec7645e4f432ab33f897e29
Daniel Herzig <daniel.herzig@univie.ac.at>
2023-12-19 18:37:51 +0100

    fix mongodump path in chronos after dockerfile installation changes.

--- container_init/chronos/mongodb-phaidra.bash
+++ container_init/chronos/mongodb-phaidra.bash
@@ -1,1 +1,1 @@
-/usr/local/bin/mongodump -h mongodb-phaidra -u ${MONGODB_PHAIDRA_USER} -p ${MONGODB_PHAIDRA_PASSWORD} --gzip --archive=/mnt/database-dumps/$(date +%F-%H-%M-%S)-mongodb.tar.gz
+/usr/bin/mongodump -h mongodb-phaidra -u ${MONGODB_PHAIDRA_USER} -p ${MONGODB_PHAIDRA_PASSWORD} --gzip --archive=/mnt/database-dumps/$(date +%F-%H-%M-%S)-mongodb.tar.gz
