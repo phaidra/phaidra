9fca1917f7d36d3777bb34a33897f26ce72e0788
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2024-02-16 11:48:59 +0100

    dont clutter UI with forbidden msgs

--- src/phaidra-ui/plugins/axios.js
+++ src/phaidra-ui/plugins/axios.js
@@ -10,7 +10,9 @@ export default function ({ store, $axios, $sentry }) {
   $axios.onError(error => {
     $sentry.captureException(error)
     if (error.response?.data?.alerts?.length > 0) {
-      store.commit('setAlerts', error.response.data.alerts)
+      if (error.response?.status !== 403) {
+        store.commit('setAlerts', error.response.data.alerts)
+      }
     }
     return Promise.reject(error)
   })
