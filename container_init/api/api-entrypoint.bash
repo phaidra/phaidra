8fe60963f7934f1e7fa6a7debd867709048fa38b
Daniel Herzig <daniel.herzig@univie.ac.at>
2024-03-05 09:47:08 +0000

    Update api-entrypoint.bash (keep log redirect to stdout -- for loki/grafana).

--- container_init/api/api-entrypoint.bash
+++ container_init/api/api-entrypoint.bash
@@ -1,5 +1,5 @@
-# if [[ ! -f /var/log/phaidra/api.log ]]
-# then
-#     ln -sF /proc/1/fd/1 /var/log/phaidra/api.log
-# fi
+if [[ ! -f /var/log/phaidra/api.log ]]
+then
+    ln -sF /proc/1/fd/1 /var/log/phaidra/api.log
+fi
 hypnotoad -f phaidra-api.cgi
