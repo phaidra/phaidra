b4a4f1b192fdd7cf46e2473119762743537451c7
Daniel Herzig <daniel.herzig@univie.ac.at>
2024-05-24 13:28:44 +0200

    increase verbosity of database-dump-cleaner chronos script.

--- container_init/chronos/database-dump-cleaner.bash
+++ container_init/chronos/database-dump-cleaner.bash
@@ -1,3 +1,23 @@
-find /mnt/database-dumps -type f -name "*${PHAIDRADB}*" -ctime +30 -delete
-find /mnt/database-dumps -type f -name "*${FEDORADB}*" -ctime +30 -delete
-find /mnt/database-dumps -type f -name "*mongodb*" -ctime +30 -delete
+printf "############################################################\n"
+printf "### cleaning up old database dumps #########################\n"
+printf "############################################################\n"
+printf "### deleting mariadb phaidra dumps: ########################\n"
+find /mnt/database-dumps -type f \
+     -name "*${PHAIDRADB}*" \
+     -ctime +30 -delete -print | \
+    grep \/ || printf "### nothing older than 30 days, doing nothing.\n"
+printf "############################################################\n"
+printf "### deleting mariadb fedora dumps: #########################\n"
+find /mnt/database-dumps -type f \
+     -name "*${FEDORADB}*" \
+     -ctime +30 -delete -print | \
+    grep \/ || printf "### nothing older than 30 days, doing nothing.\n"
+printf "############################################################\n"
+printf "### deleting old mongodb phaidra dumps: ####################\n"
+find /mnt/database-dumps -type f \
+     -name "*mongodb*" \
+     -ctime +30 -delete -print | \
+    grep \/ || printf "### nothing older than 30 days, doing nothing.\n"
+printf "############################################################\n"
+printf "### done. ##################################################\n"
+printf "############################################################\n"
