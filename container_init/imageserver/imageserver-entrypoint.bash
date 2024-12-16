db8c949bfbfdbd16cdb4f6662f6936c774a41e98
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2024-12-16 17:18:32 +0100

    httpd startup

--- container_init/imageserver/imageserver-entrypoint.bash
+++ container_init/imageserver/imageserver-entrypoint.bash
@@ -1,3 +1,2 @@
 echo "FcgidInitialEnv BASE_URL \"${OUTSIDE_HTTP_SCHEME}://${PHAIDRA_HOSTNAME}${PHAIDRA_PORTSTUB}${PHAIDRA_HOSTPORT}/api/imageserver?IIIF=\"" >> /etc/apache2/mods-enabled/iipsrv.conf
-/etc/init.d/shibd start
-httpd-foreground
+/usr/sbin/apachectl -DFOREGROUND
