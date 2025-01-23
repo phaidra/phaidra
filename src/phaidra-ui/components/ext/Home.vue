b037536c9d723f42502ce7a84d3b8c4682aca48c
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-01-23 11:54:30 +0100

    autoplay wont work because refs do not seem to work with runtime template

--- src/phaidra-ui/components/ext/Home.vue
+++ src/phaidra-ui/components/ext/Home.vue
@@ -122,53 +122,9 @@ export default {
   mixins: [config],
   data() {
     return {
-      q: '',
-      hasPrev: false,
-      hasNext: false,
-      interval: null,
-      // relative width to move when next/prev is clicked.
-      displacement: 1.0,
+      q: ''
     }
-  },
-  methods: {
-    onScrollDebounce({hasNext, hasPrev}) {
-      this.hasPrev = hasPrev
-      this.hasNext = hasNext
-    },
-    play() {
-      if (!this.hasNext && this.hasPrev) {
-        this.$refs.horizontal.scrollToIndex(0)
-        this.displacement = 1.0
-        return
-      }
-
-      if (this.hasNext) {
-        this.$refs.horizontal.next()
-
-        // After first nav, change displacement window to just 60%
-        this.displacement = 0.6
-      }
-    }
-  },
-  mounted() {
-    observeVisibility(this.$refs.horizontal.$el, (visible) => {
-      if (visible) {
-        this.interval = setInterval(this.play, 3000)
-      } else {
-        clearInterval(this.interval)
-      }
-    })
-  },
-  destroyed() {
-    clearInterval(this.interval)
   }
-}
-
-function observeVisibility(element, callback) {
-  const observer = new IntersectionObserver((records) => {
-    callback(records.find(record => record.isIntersecting))
-  }, {rootMargin: '10% 0% 10% 0%', threshold: 1.0});
-  observer.observe(element);
 }
 </script>
 
