0c0d3c2feb58e9939ab4619b60426558bc8db32e
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-01-23 11:35:32 +0100

    slider-support-for-home

--- src/phaidra-ui/components/ext/Home.vue
+++ src/phaidra-ui/components/ext/Home.vue
@@ -112,15 +112,63 @@ import '@/compiled-icons/univie-youtube'
 import '@/compiled-icons/univie-twitter'
 import '@/compiled-icons/univie-instagram'
 import '@/compiled-icons/univie-flickr'
+import VueHorizontal from 'vue-horizontal'
 
 export default {
   name: 'home',
+  components: {
+    VueHorizontal
+  },
   mixins: [config],
   data() {
     return {
-      q: ''
+      q: '',
+      hasPrev: false,
+      hasNext: false,
+      interval: null,
+      // relative width to move when next/prev is clicked.
+      displacement: 1.0,
     }
+  },
+  methods: {
+    onScrollDebounce({hasNext, hasPrev}) {
+      this.hasPrev = hasPrev
+      this.hasNext = hasNext
+    },
+    play() {
+      if (!this.hasNext && this.hasPrev) {
+        this.$refs.horizontal.scrollToIndex(0)
+        this.displacement = 1.0
+        return
+      }
+
+      if (this.hasNext) {
+        this.$refs.horizontal.next()
+
+        // After first nav, change displacement window to just 60%
+        this.displacement = 0.6
+      }
+    }
+  },
+  mounted() {
+    observeVisibility(this.$refs.horizontal.$el, (visible) => {
+      if (visible) {
+        this.interval = setInterval(this.play, 3000)
+      } else {
+        clearInterval(this.interval)
+      }
+    })
+  },
+  destroyed() {
+    clearInterval(this.interval)
   }
+}
+
+function observeVisibility(element, callback) {
+  const observer = new IntersectionObserver((records) => {
+    callback(records.find(record => record.isIntersecting))
+  }, {rootMargin: '10% 0% 10% 0%', threshold: 1.0});
+  observer.observe(element);
 }
 </script>
 
@@ -139,4 +187,5 @@ h2.md-headline {
   top: -24px;
   position: relative;
 }
+
 </style>
