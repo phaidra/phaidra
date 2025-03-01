f0397664266498538d71e166ce4cb4d9a8bfbc84
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-03-01 05:29:52 +0000

    error page

--- src/phaidra-ui/layouts/error.vue
+++ src/phaidra-ui/layouts/error.vue
@@ -1,7 +1,21 @@
-<template></template>
+<template>
+   <div class="pt-4">
+      <div v-if="error.statusCode === 404">
+        <h1>{{$t('This page does not exist')}}...</h1>
+        <p>{{$t('We apologise for the inconvenience, the page you are trying to access does not exist at this address.')}}</p>
+        <p v-if="instanceconfig.email">{{$t('If you are sure you entered the correct address but still get an error, please contact')}} <a :href="'mailto:' + instanceconfig.email">{{ instanceconfig.email }}</a></p>
+      </div>
+      <h1 v-else>An error occurred</h1>
+      <p>{{$t('Return to')}} <NuxtLink to="/">Home page</NuxtLink></p>
+  </div>
+</template>
 
 <script>
+import { config } from "@/mixins/config";
 export default {
+  props: ['error'],
+  layout: 'error',
+  mixins: [config]
   // beforeMount() {
   //   this.$router.push(this.localeLocation('/notfound'))
   // }
