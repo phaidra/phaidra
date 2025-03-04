16a2e24014df822faa17f1791024c132551e3d4d
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-03-04 09:20:01 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Giulio Turetta <giulio.turetta@unipd.it>

--- src/phaidra-ui/layouts/error.vue
+++ src/phaidra-ui/layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="pt-4">
       <div v-if="error.statusCode === 404">
-        <h1>{{$t('This page does not exist')}}...</h1>
+        <h1>{{$t('This page does not exist...')}}</h1>
         <p>{{$t('We apologise for the inconvenience, the page you are trying to access does not exist at this address.')}}</p>
         <p v-if="instanceconfig.email">{{$t('If you are sure you entered the correct address but still get an error, please contact')}} <a :href="'mailto:' + instanceconfig.email">{{ instanceconfig.email }}</a></p>
       </div>
