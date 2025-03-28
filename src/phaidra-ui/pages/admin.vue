c7d1a0cabee780ade2bd73b12807519c3a9ded0f
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-03-28 14:28:36 +0000

    text change on admin conf for force preview

--- src/phaidra-ui/pages/admin.vue
+++ src/phaidra-ui/pages/admin.vue
@@ -228,11 +228,11 @@
                 <v-row>
                   <v-col>
                     <v-checkbox
-                      :label="$t('Enable force preview')"
+                      :label="$t('Upload preview')"
                       v-model="parsedPublicConfigData.forcePreview"
                     ></v-checkbox>
                   </v-col>
-                  <v-col cols="6" class="mt-4">{{ $t("Enable preview and upload in Upload form.") }}</v-col>
+                  <v-col cols="6" class="mt-4">{{ $t("Move upload button to preview tab in submit form.") }}</v-col>
                 </v-row>
                 <v-row>
                   <v-col>
