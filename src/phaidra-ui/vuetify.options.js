9d2c8f4433d7c626a469ba45bc03da2a921c50cb
Giulio Turetta <giulio.turetta@unipd.it>
2025-04-01 13:48:52 +0000

    add secondary color to Vuetify config

--- src/phaidra-ui/vuetify.options.js
+++ src/phaidra-ui/vuetify.options.js
@@ -4,10 +4,12 @@ export default function ({ app }) {
       themes: {
         light: {
           primary: app.$config.primaryColor,
-          error: '#dd4814'
+          secondary: '#616161', // grey darken-2
+          error: '#dd4814',          
         },
         dark: {
           primary: app.$config.primaryColor,
+          secondary: '#e2e2e2',
           error: '#dd4814'
         }
       }
