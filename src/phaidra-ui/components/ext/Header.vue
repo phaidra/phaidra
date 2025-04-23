c3c76ff8dd85e45084e46d35c61da6e6c825e545
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-04-23 07:01:22 +0000

    Shibboleth sso logout hide for screenreader

--- src/phaidra-ui/components/ext/Header.vue
+++ src/phaidra-ui/components/ext/Header.vue
@@ -580,7 +580,7 @@
           </v-row>
           </v-col>
       </v-row>
-      <a ref="logoutlink" href="/Shibboleth.sso/Logout"></a>
+      <a ref="logoutlink" href="/Shibboleth.sso/Logout" aria-hidden="true" role="presentation" tabindex="-1"></a>
     </div>
   </template>
   
