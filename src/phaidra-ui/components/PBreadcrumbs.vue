e83b73c48a7506c277443eca28c73b2ff55b4af7
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-04-24 08:49:47 +0000

    breadcrumb progress change

--- src/phaidra-ui/components/PBreadcrumbs.vue
+++ src/phaidra-ui/components/PBreadcrumbs.vue
@@ -1,39 +1,42 @@
 <template>
   <v-row>
-    <v-col>
+    <v-col cols="12" class="breadcrumbs-container pb-0" aria-label="Breadcrumbs">
       <span class="youarehere secondary--text">{{ $t("You are here") }}:</span>
-      <template v-for="(item, index) in translatedArray">
-        <icon
-          :key="'icon' + index"
-          left
-          dark
-          name="univie-right"
-          color="#a4a4a4"
-          width="8px"
-          height="8px"
-          class="mx-1"
-        ></icon>
-        <span :key="'distext' + index" v-if="item.disabled" class="text">{{
-          $t(item.text)
-        }}</span>
-        <template v-else>
-          <a
-            :key="'iconex' + index"
-            v-if="item.external"
-            :href="item.to"
-            class="text"
-            >{{ $t(item.text) }}</a
-          >
-          <nuxt-link
-            :key="'link' + index"
-            v-else
-            :to="item.to"
-            class="text"
-            >{{ $t(item.text) }}</nuxt-link
-          >
+      <ol class="breadcrumbs-list">
+        <template v-for="(item, index) in translatedArray">
+          <li :key="'item' + index" class="breadcrumb-item" :aria-current="item.disabled ? 'page' : undefined">
+            <icon
+              v-if="index > 0"
+              :key="'icon' + index"
+              left
+              dark
+              name="univie-right"
+              color="#a4a4a4"
+              width="8px"
+              height="8px"
+              class="mx-1"
+            ></icon>
+            <span v-if="item.disabled" class="text">{{ $t(item.text) }}</span>
+            <template v-else>
+              <a
+                v-if="item.external"
+                :href="item.to"
+                class="text"
+                >{{ $t(item.text) }}</a
+              >
+              <nuxt-link
+                v-else
+                :to="item.to"
+                class="text"
+                >{{ $t(item.text) }}</nuxt-link
+              >
+            </template>
+          </li>
         </template>
-      </template>
+      </ol>
       <v-divider></v-divider>
+    </v-col>
+    <v-col cols="12" class="py-0">
       <v-progress-linear
         :active="$store.state.loading"
         :height="2"
@@ -74,5 +77,16 @@ export default {
 
 .text {
   font-size: 10pt;
+}
+
+.breadcrumbs-list {
+  display: inline;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.breadcrumb-item {
+  display: inline;
 }
 </style>
