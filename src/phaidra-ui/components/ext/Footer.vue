c5fa5ed5eae6712372772caf6fbd07fb52db3351
Giulio Turetta <giulio.turetta@unipd.it>
2025-05-13 15:26:08 +0200

    footer margins and i18n

--- src/phaidra-ui/components/ext/Footer.vue
+++ src/phaidra-ui/components/ext/Footer.vue
@@ -2,16 +2,16 @@
   <div v-if="instanceconfig.cms_footer">
     <runtimetemplate :template="instanceconfig.cms_footer" />
   </div>
-  <v-row v-else>
+  <v-row v-else class="my-6">
     <v-col cols="12" md="10" offset-md="1">
-      <v-row class="my-5">
-        <v-col cols="auto" class="text-left">
+      <v-row>
+        <v-col cols="auto">
           <address>
-            {{ instanceconfig.address }} | <abbr title="Telefon">T</abbr>
+            {{ instanceconfig.address }} | <abbr :title="$t('Telephone')">T</abbr>
             {{ instanceconfig.phone }}
           </address>
         </v-col>
-        <v-col class="text-right">
+        <v-col class="text-md-right">
           <nuxt-link :to="localePath('impressum')">{{ $t('Impressum') }}</nuxt-link> |
           <nuxt-link :to="localePath('termsofuse')">{{ $t('Terms of Use') }}</nuxt-link> |
           <nuxt-link :to="localePath('credits')">{{ $t('Credits') }}</nuxt-link>
