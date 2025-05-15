90816243159ff268a66fa53c0f78c51f5e5fed5f
giulioturetta <giulio.turetta@unipd.it>
2025-05-15 15:37:56 +0200

    show 2-letter language code on uwmetadata objects title

--- src/phaidra-vue-components/src/components/legacy/PDUwmRec.vue
+++ src/phaidra-vue-components/src/components/legacy/PDUwmRec.vue
@@ -8,7 +8,7 @@
       </template>
       <template v-else-if="ch.input_type === 'input_text'">
         <template v-if="nodePath(ch) === 'uwm_general_title'">
-          <v-col cols="12" md="2" class="pdlabel secondary--text font-weight-bold text-md-right">{{ $t(nodePath(ch)) }}</v-col>
+          <v-col cols="12" md="2" class="pdlabel secondary--text font-weight-bold text-md-right">{{ $t(nodePath(ch)) }}<template v-if="getLangAttr(ch)"> ({{getLangAttr(ch)}})</template></v-col>
           <v-col cols="12" md="10" class="wiv">{{ ch.ui_value }}</v-col>
         </template>
         <template v-else-if="nodePath(ch) === 'uwm_lifecycle_upload_date'">
