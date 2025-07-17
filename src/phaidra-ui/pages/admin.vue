92cea7958dbbcaa4bf90511d6416c3b4d18adb62
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-07-17 13:07:06 +0530

    hide / show institution name

--- src/phaidra-ui/pages/admin.vue
+++ src/phaidra-ui/pages/admin.vue
@@ -231,6 +231,15 @@
                 <v-row>
                   <v-col>
                     <v-checkbox
+                      :label="$t('Hide institution name by default.')"
+                      v-model="parsedPublicConfigData.hideInstitutionName"
+                    ></v-checkbox>
+                  </v-col>
+                  <v-col cols="6" class="mt-4">{{ $t("Show/Hide institution name in breadcrumbs.") }}</v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-checkbox
                       :label="$t('Upload preview')"
                       v-model="parsedPublicConfigData.forcePreview"
                     ></v-checkbox>
