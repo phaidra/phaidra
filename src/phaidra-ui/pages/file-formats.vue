91c7230eaa9d09d51c19f30c5b8e6840dbe657f4
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-09-19 18:43:45 +0530

    added file formats cms page

---
+++ src/phaidra-ui/pages/file-formats.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1 class="d-sr-only">{{$t('File Formats')}}</h1>
+      <ExtFileFormats></ExtFileFormats>
+    </div>
+  </template>
+    
+    <script>
+    import { config } from "../mixins/config";
+    
+    export default {
+      mixins: [config],
+      metaInfo() {
+        let metaInfo = {
+          title: this.$t('File Formats') + ' - ' + this.$t(this.instanceconfig.title) + ' - ' + this.$t(this.instanceconfig.institution),
+        };
+        return metaInfo;
+      },
+    }
+    </script>
+    
