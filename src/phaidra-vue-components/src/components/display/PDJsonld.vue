ab02d9deadcf1163fbe7af2092fd60ed3f185162
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-10-09 17:39:24 +0530

    added diameter abd depth field and resolved duplicate key issue

--- src/phaidra-vue-components/src/components/display/PDJsonld.vue
+++ src/phaidra-vue-components/src/components/display/PDJsonld.vue
@@ -299,7 +299,7 @@
         </template>
 
         <template v-else-if="p==='vra:diameter'" slot="vra:diameter">
-          <p-d-dimension :p="p" :o="item" v-for="(item, j) in o" :key="componentid+'depth'+j" v-bind.sync="displayProperties"></p-d-dimension>
+          <p-d-dimension :p="p" :o="item" v-for="(item, j) in o" :key="componentid+'diameter'+j" v-bind.sync="displayProperties"></p-d-dimension>
         </template>
 
         <template v-else-if="p==='schema:weight'" slot="schema:weight">
