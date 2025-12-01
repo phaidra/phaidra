e1682d20586e18dbedb645bade0ae04eeff848f0
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2025-12-01 15:30:34 +0530

    is in series menu condition fix

--- src/phaidra-vue-components/src/components/input/PISeries.vue
+++ src/phaidra-vue-components/src/components/input/PISeries.vue
@@ -6,7 +6,7 @@
         <v-card-title class="title font-weight-light white--text">
           <span>{{ $t(label) }}</span>
           <v-spacer></v-spacer>
-          <v-menu bottom offset-y>
+          <v-menu v-if="multiplicable || multiplicableCleared || removable" bottom offset-y>
             <template v-slot:activator="{ on, attrs }">
               <v-btn v-on="on" v-bind="attrs" icon dark>
                 <v-icon dark>mdi-dots-vertical</v-icon>
