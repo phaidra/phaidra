07fa41e55d4f06ef0235f9aef28ab30c26c598ea
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-12-09 11:40:50 +0100

    fix wrong collection name in init script

--- container_init/mongodb/mongodb-after-entry.sh
+++ container_init/mongodb/mongodb-after-entry.sh
@@ -37,7 +37,7 @@ db.oai_records.deleteOne({ _temp: true });
 echo "Adding indexes to jobs collection..."
 mongo --quiet --authenticationDatabase admin -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} paf_mongodb --eval "
 db = db.getSiblingDB('paf_mongodb');
-if (db.jobs.countDocuments({}) === 0) db.oai_records.insertOne({ _temp: true });
+if (db.jobs.countDocuments({}) === 0) db.jobs.insertOne({ _temp: true });
 db.jobs.createIndexes([{ agent_name: 1 }, { status: 1 }, { idhash: 1 }] );
 db.jobs.deleteOne({ _temp: true });
 "
