38ec2e51a7eb2fcc44341de7b99f3bb991f1ffb1
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-12-16 19:51:21 +0100

    Make httpd configurable via envs

--- container_init/httpd/phaidra-ssl/httpd-ssl-entrypoint.bash
+++ container_init/httpd/phaidra-ssl/httpd-ssl-entrypoint.bash
@@ -1,3 +1,9 @@
-cp /ssl/fullchain.pem /usr/local/apache2/conf/server.crt
-cp /ssl/privkey.pem /usr/local/apache2/conf/server.key
-httpd-foreground
+EXTRA_FLAGS=
+if [ "${HTTPD_ACME_ENABLE}" = "true" ]; then
+  EXTRA_FLAGS="-D ACME"
+else
+  cp /ssl/fullchain.pem /usr/local/apache2/conf/server.crt
+  cp /ssl/privkey.pem /usr/local/apache2/conf/server.key
+fi
+exec httpd-foreground ${EXTRA_FLAGS}
+
