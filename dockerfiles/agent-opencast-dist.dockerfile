f20807ece798e1fdf6ef7f5ba2e40b8e63423c04
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-12-20 16:04:26 +0100

    Fix missing agent renames

--- dockerfiles/agent-opencast-dist.dockerfile
+++ dockerfiles/agent-opencast-dist.dockerfile
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 FROM phaidraorg/agent-opencast-base:latest
-ADD ../src/agents/opencast /mnt/opencast/
-=======
-FROM phaidraorg/vige-base:latest
-ADD ../src/agents/vige /mnt/vige/
->>>>>>> ce525203 (Rebase to main and normalize unzip agent and volume)
+ADD ../src/agents/opencast /mnt/agent-opencast/
--- src/agents/opencast/controller.bash
+++ src/agents/opencast/controller.bash
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 parallel --lb ::: 'bash /mnt/agent-opencast/checker.bash' 'bash /mnt/agent-opencast/uploader.bash' 'bash /mnt/agent-opencast/cleaner.bash'
-=======
-parallel --lb ::: 'bash /mnt/vige/checker.bash' 'bash /mnt/vige/uploader.bash' 'bash /mnt/vige/cleaner.bash'
->>>>>>> ce525203 (Rebase to main and normalize unzip agent and volume)
