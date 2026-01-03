06cae17689d3fbca5bc7feefd96a038329582716
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2026-01-03 14:31:13 +0100

    Remove assigning country codes for now

--- container_init/chronos/original-perl/processUsageStats.pl
+++ container_init/chronos/original-perl/processUsageStats.pl
@@ -43,69 +43,69 @@ sub delete_offender_visitors {
   return $sth->rows;  # affected rows (driver reports delete count)
 }
 
-# Assign country codes
-# Taking 3 day window to allow for some overlap... and won't repeatedly try if it does not work
-# though if everything works well, only the one from last day should be NULL anyway
-sub assign_countries_v4 {
-  my ($dbh, $overlap_days) = @_;
-  die "overlap_days must be > 0" unless $overlap_days && $overlap_days > 0;
+# # Assign country codes
+# # Taking 3 day window to allow for some overlap... and won't repeatedly try if it does not work
+# # though if everything works well, only the one from last day should be NULL anyway
+# sub assign_countries_v4 {
+#   my ($dbh, $overlap_days) = @_;
+#   die "overlap_days must be > 0" unless $overlap_days && $overlap_days > 0;
 
-  my $sql = q{
-    UPDATE usage_log AS e
-    JOIN ip2location_v4 AS l
-      ON e.ip_version = 4
-     AND e.ip_v4 BETWEEN l.start_ip AND l.end_ip
-    SET e.country = CASE
-                      WHEN l.country IS NULL OR l.country = 'None' THEN 'xx'
-                      ELSE LOWER(l.country)
-                    END
-    WHERE e.country IS NULL
-      AND e.created >= DATE_SUB(NOW(), INTERVAL ? DAY)
-  };
+#   my $sql = q{
+#     UPDATE usage_log AS e
+#     JOIN ip2location_v4 AS l
+#       ON e.ip_version = 4
+#      AND e.ip_v4 BETWEEN l.start_ip AND l.end_ip
+#     SET e.country = CASE
+#                       WHEN l.country IS NULL OR l.country = 'None' THEN 'xx'
+#                       ELSE LOWER(l.country)
+#                     END
+#     WHERE e.country IS NULL
+#       AND e.created >= DATE_SUB(NOW(), INTERVAL ? DAY)
+#   };
 
-  my $sth = $dbh->prepare($sql);
-  $sth->execute($overlap_days);
-  return $sth->rows;  # rows updated
-}
+#   my $sth = $dbh->prepare($sql);
+#   $sth->execute($overlap_days);
+#   return $sth->rows;  # rows updated
+# }
 
-sub assign_countries_v6 {
-  my ($dbh, $overlap_days) = @_;
-  die "overlap_days must be > 0" unless $overlap_days && $overlap_days > 0;
+# sub assign_countries_v6 {
+#   my ($dbh, $overlap_days) = @_;
+#   die "overlap_days must be > 0" unless $overlap_days && $overlap_days > 0;
 
-  my $sql = q{
-    UPDATE usage_log AS e
-    JOIN ip2location_v6 AS l
-      ON e.ip_version = 6
-     AND e.ip_v6 BETWEEN l.start_ip AND l.end_ip
-    SET e.country = CASE
-                      WHEN l.country IS NULL OR l.country = 'None' THEN 'xx'
-                      ELSE LOWER(l.country)
-                    END
-    WHERE e.country IS NULL
-      AND e.created >= DATE_SUB(NOW(), INTERVAL ? DAY)
-  };
+#   my $sql = q{
+#     UPDATE usage_log AS e
+#     JOIN ip2location_v6 AS l
+#       ON e.ip_version = 6
+#      AND e.ip_v6 BETWEEN l.start_ip AND l.end_ip
+#     SET e.country = CASE
+#                       WHEN l.country IS NULL OR l.country = 'None' THEN 'xx'
+#                       ELSE LOWER(l.country)
+#                     END
+#     WHERE e.country IS NULL
+#       AND e.created >= DATE_SUB(NOW(), INTERVAL ? DAY)
+#   };
 
-  my $sth = $dbh->prepare($sql);
-  $sth->execute($overlap_days);
-  return $sth->rows;  # rows updated
-}
+#   my $sth = $dbh->prepare($sql);
+#   $sth->execute($overlap_days);
+#   return $sth->rows;  # rows updated
+# }
 
-sub assign_countries {
-  my ($dbh, $overlap_days) = @_;
-  my ($v4_rows, $v6_rows) = (0, 0);
-  eval {
-    $dbh->begin_work;
-    $v4_rows = assign_countries_v4($dbh, $overlap_days);
-    $v6_rows = assign_countries_v6($dbh, $overlap_days);
-    $dbh->commit;
-    1;
-  } or do {
-    my $err = $@ || 'unknown error';
-    eval { $dbh->rollback };
-    die "assign_countries failed: $err";
-  };
-  return ($v4_rows, $v6_rows);
-}
+# sub assign_countries {
+#   my ($dbh, $overlap_days) = @_;
+#   my ($v4_rows, $v6_rows) = (0, 0);
+#   eval {
+#     $dbh->begin_work;
+#     $v4_rows = assign_countries_v4($dbh, $overlap_days);
+#     $v6_rows = assign_countries_v6($dbh, $overlap_days);
+#     $dbh->commit;
+#     1;
+#   } or do {
+#     my $err = $@ || 'unknown error';
+#     eval { $dbh->rollback };
+#     die "assign_countries failed: $err";
+#   };
+#   return ($v4_rows, $v6_rows);
+# }
 
 sub aggregate_usage_log_into_usage_statistics {
   my ($dbh) = @_;
@@ -172,8 +172,8 @@ sub aggregate_usage_log_into_usage_statistics {
 my $deleted = delete_offender_visitors($dbh, $overlap_days, $bot_threshold);
 printf "Deleted %d offender rows\n", $deleted;
 
-my ($v4u, $v6u) = assign_countries($dbh, $overlap_days);
-printf "Assigned country: v4=%d, v6=%d rows\n", $v4u, $v6u;
+# my ($v4u, $v6u) = assign_countries($dbh, $overlap_days);
+# printf "Assigned country: v4=%d, v6=%d rows\n", $v4u, $v6u;
 
 my ($affected, $from_id, $to_id) = aggregate_usage_log_into_usage_statistics($dbh);
 printf "Aggregated window id > %d and id <= %d; affected rows: %d\n", $from_id, $to_id, $affected;
