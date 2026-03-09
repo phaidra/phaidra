1cfc7545c4a3e384caf363e75eb5017d4f09849f
Giulio Turetta <giulio.turetta@unipd.it>
2026-03-09 11:35:29 +0100

    italian translation fix

--- src/phaidra-api/PhaidraAPI/Model/Uwmetadata/Tree.pm
+++ src/phaidra-api/PhaidraAPI/Model/Uwmetadata/Tree.pm
@@ -3141,7 +3141,7 @@ our %tree = (
           "labels"      => {
             "de" => "Beschreibung oder zus\x{e4}tzliche Angaben",
             "en" => "Description or Additional Data",
-            "it" => "Descrizione dei diritti",
+            "it" => "Informazioni sui diritti",
             "sr" => "opis ili dodatni podaci"
           },
           "loaded" => 0,
