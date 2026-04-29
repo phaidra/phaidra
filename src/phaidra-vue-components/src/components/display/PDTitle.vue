bbc8d034fb0703a00f3745acfe6511d27fbb7c00
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-04-29 18:06:51 +0530

    title template fix

--- src/phaidra-vue-components/src/components/display/PDTitle.vue
+++ src/phaidra-vue-components/src/components/display/PDTitle.vue
@@ -1,19 +1,17 @@
 <template>
-  <span>
-    <template>
-      <v-row v-for="(mt, i) in o['bf:mainTitle']" :key="'mt'+i">
-        <v-col :md="labelColMd" cols="12" class="pdlabel text-secondary font-weight-bold text-md-right">
-          {{ $t(o['@type']) }}<template v-if="showLang && mt['@language']"> ({{ mt['@language'] }})</template>
-        </v-col>
-        <v-col :md="valueColMd" cols="12">
-            <v-row no-gutters class="valuefield" :class="{ 'font-weight-regular': boldLabelFields.includes('dce:title') }">{{ mt['@value'] }}</v-row>
-            <template v-for="(st, si) in o['bf:subtitle']" :key="'stv' + i + '-' + si">
-              <v-row no-gutters class="valuefield">{{ st['@value'] }}</v-row>
-            </template>
-        </v-col>
-      </v-row>
-    </template>
-  </span>
+  <div>
+    <v-row v-for="(mt, i) in o['bf:mainTitle']" :key="'mt'+i">
+      <v-col :md="labelColMd" cols="12" class="pdlabel text-secondary font-weight-bold text-md-right">
+        {{ $t(o['@type']) }}<template v-if="showLang && mt['@language']"> ({{ mt['@language'] }})</template>
+      </v-col>
+      <v-col :md="valueColMd" cols="12">
+          <v-row no-gutters class="valuefield" :class="{ 'font-weight-regular': boldLabelFields.includes('dce:title') }">{{ mt['@value'] }}</v-row>
+          <template v-for="(st, si) in o['bf:subtitle']" :key="'stv' + i + '-' + si">
+            <v-row no-gutters class="valuefield">{{ st['@value'] }}</v-row>
+          </template>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
