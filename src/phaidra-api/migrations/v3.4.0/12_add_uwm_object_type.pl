0f7c2768c0e12fa30d2c7137b3af5a8148c0b69c
giulioturetta <giulio.turetta@unipd.it>
2026-05-06 16:50:45 +0200

    add migration script for sound cassette uwm vocab entry

---
+++ src/phaidra-api/migrations/v3.4.0/12_add_uwm_object_type.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Log::Log4perl;
+use DBIx::Connector;
+
+# Usage: docker exec -it phaidra-api-1 perl migrations/<VERSION>/<SCRIPT>.pl
+
+$ENV{MOJO_INACTIVITY_TIMEOUT} = 36000;
+
+my $logconf = q(
+  log4perl.category.MyLogger         = INFO, Screen
+
+  log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+  log4perl.appender.Screen.stderr  = 0
+  log4perl.appender.Screen.layout  = Log::Log4perl::Layout::PatternLayout
+  log4perl.appender.Screen.layout.ConversionPattern=%d %m%n
+  log4perl.appender.Screen.utf8   = 1
+);
+
+Log::Log4perl::init(\$logconf);
+my $log = Log::Log4perl::get_logger("MyLogger");
+
+$log->info("started migration to v3.4.0");
+
+my $cntr = DBIx::Connector->new("dbi:mysql:phaidradb:" . $ENV{MARIADB_PHAIDRA_HOST}, $ENV{MARIADB_PHAIDRA_USER}, $ENV{MARIADB_PHAIDRA_PASSWORD}, {mysql_auto_reconnect => 1, mysql_multi_statements => 1});
+$cntr->mode('ping');
+
+$cntr->dbh->do("
+
+  -- object type added in Padova
+  INSERT INTO vocabulary_entry (veid, isocode, entry, vid) VALUES(1557168,'it','audiocassetta',24);
+  INSERT INTO vocabulary_entry (veid, isocode, entry, vid) VALUES(1557168,'en','sound cassette',24);
+  INSERT INTO vocabulary_entry (veid, isocode, entry, vid) VALUES(1557168,'de','Ton-Kassette',24);
+
+  ");
+
+$log->info("finished migration to v3.4.0");
+
+__END__
+
+
