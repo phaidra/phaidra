ad1a025c171d255d4fee93c7346527ba8b935615
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2026-06-12 10:53:31 +0200

    Remove univie specific code from help page

--- src/phaidra-vue-components/src/components/info/PHelp.vue
+++ src/phaidra-vue-components/src/components/info/PHelp.vue
@@ -4,21 +4,9 @@
     <v-card-text class="mt-4">
       <div v-if="$i18n.locale === 'deu'">
         <p><nuxt-link :to="'/metadata-fields-help'" target="_blank">Beschreibung aller vorhandenen Metadatenfelder</nuxt-link></p>
-        <template v-if="instanceconfig.baseurl.includes('univie.ac.at')">
-          <p>Weiterführende Links:</p>
-          <p><a href="https://datamanagement.univie.ac.at/ueber-phaidra-services/downloads-und-anleitungen/" target="_blank">PHAIDRA User Guide und Anleitungen</a></p>
-          <p><a href="https://rdm.univie.ac.at/de/fdm-policy-und-faq/" target="_blank">Forschungsdaten Policy an der Universität Wien</a></p>
-          <p><a href="https://doi-service.univie.ac.at/" target="_blank">DOI Service der Universität Wien</a></p>
-        </template>
       </div>
       <div v-else>
         <p><nuxt-link :to="'/metadata-fields-help'" target="_blank">Description of all existing metadata fields</nuxt-link></p>
-        <template v-if="instanceconfig.baseurl.includes('univie.ac.at')">
-          <p>Related Links:</p>
-          <p><a href="https://datamanagement.univie.ac.at/en/about-phaidra/downloads/" target="_blank">PHAIDRA User Guide and Manuals</a></p>
-          <p><a href="https://rdm.univie.ac.at/rdm-policy-and-faq/" target="_blank">RDM Policy at University of Vienna</a></p>
-          <p><a href="https://doi-service.univie.ac.at/en/" target="_blank">DOI service of the University of Vienna</a></p>
-        </template>
       </div>
     </v-card-text>
   </v-card>
@@ -37,7 +25,7 @@ export default {
       return this.$root.$store.state.instanceconfig
     }
   },
-  data () {
+  data() {
     return {
       loading: false
     }
