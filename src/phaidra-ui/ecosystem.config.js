0e7c77274437c0205c754e9983c1a0f6bba2aa1f
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2026-06-19 14:21:34 +0200

    Fix nuxt-4 ui image

--- src/phaidra-ui/ecosystem.config.js
+++ src/phaidra-ui/ecosystem.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'nuxt',
-    cwd: '/usr/local/phaidra/phaidra-ui',
-    script: './node_modules/.bin/nuxt',
-    args: `start -c ${process.cwd()}/nuxt.config.js`,
-    exec_mode: 'cluster',
-    instances: '2',
-    watch: false,
-    env: { NODE_ENV: 'production' }
-  }]
-}
+  apps: [
+    {
+      name: 'nuxt',
+      cwd: '/usr/local/phaidra/phaidra-ui',
+      script: '.output/server/index.mjs',
+      interpreter: 'node',
+      exec_mode: 'cluster',
+      instances: 2,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        HOST: '0.0.0.0',
+        PORT: '3001'
+      }
+    }
+  ]
+};
