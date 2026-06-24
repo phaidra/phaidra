afa521ae509c587b92346e04620b0b0fd85a84db
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-06-24 10:36:08 +0530

    runtime fix for template

--- src/phaidra-ui/components/Runtimetemplate.vue
+++ src/phaidra-ui/components/Runtimetemplate.vue
@@ -2,6 +2,17 @@
 import { h, defineComponent } from 'vue'
 import { compile } from '@vue/compiler-dom'
 import * as runtimeDom from '@vue/runtime-dom'
+import { ClientOnly, NuxtLink } from '#components'
+import Icon from './Icon.vue'
+
+const runtimeComponents = {
+  NuxtLink,
+  'nuxt-link': NuxtLink,
+  ClientOnly,
+  'client-only': ClientOnly,
+  Icon,
+  icon: Icon
+};
 
 const defineDescriptor = (src, dest, name) => {
     if (!dest.hasOwnProperty(name)) {
@@ -18,12 +29,6 @@ const defineDescriptor = (src, dest, name) => {
           defineDescriptor(obj, res, name)
         );
     });
-    return res;
-  };
-  
-  const buildFromProps = (obj, props) => {
-    const res = {};
-    props.forEach(prop => defineDescriptor(obj, res, prop));
     return res;
   };
 
@@ -52,74 +57,32 @@ export default {
       if (this.template) {
         const parent = this.parent || this.$parent
         const {
-          $data: parentData = {},
-          $props: parentProps = {},
           $options: parentOptions = {}
         } = parent;
         const {
-          components: parentComponents = {},
-          computed: parentComputed = {},
-          methods: parentMethods = {}
+          components: parentComponents = {}
         } = parentOptions;
-        const {
-          $data = {},
-          $props = {},
-          $options: { methods = {}, computed = {}, components = {} } = {}
-        } = this;
-        const passthrough = {
-          $data: {},
-          $props: {},
-          $options: {},
-          components: {},
-          computed: {},
-          methods: {}
-        };
-  
-        //build new objects by removing keys if already exists (e.g. created by mixins)
-        Object.keys(parentData).forEach(e => {
-          if (typeof $data[e] === "undefined")
-            passthrough.$data[e] = parentData[e];
-        });
-        Object.keys(parentProps).forEach(e => {
-          if (typeof $props[e] === "undefined")
-            passthrough.$props[e] = parentProps[e];
-        });
-        Object.keys(parentMethods).forEach(e => {
-          if (typeof methods[e] === "undefined")
-            passthrough.methods[e] = parentMethods[e];
-        });
-        Object.keys(parentComputed).forEach(e => {
-          if (typeof computed[e] === "undefined")
-            passthrough.computed[e] = parentComputed[e];
-        });
-        Object.keys(parentComponents).forEach(e => {
-          if (typeof components[e] === "undefined")
-            passthrough.components[e] = parentComponents[e];
-        });
-  
-        const methodKeys = Object.keys(passthrough.methods || {});
-        const dataKeys = Object.keys(passthrough.$data || {});
-        const propKeys = Object.keys(passthrough.$props || {});
-        const templatePropKeys = Object.keys(this.templateProps);
-        const allKeys = dataKeys.concat(propKeys).concat(methodKeys).concat(templatePropKeys);
-        const methodsFromProps = buildFromProps(parent, methodKeys);
-        const finalProps = merge([
-          passthrough.$data,
-          passthrough.$props,
-          methodsFromProps,
-          this.templateProps
-        ]);
+        const components = { ...runtimeComponents, ...parentComponents };
         const provide = this.$parent?._provided ?? this.$parent?.$?.provides;
+        const renderFn = compileTemplate(this.template);
+        const templateProps = this.templateProps;
   
         const dynamic = defineComponent({
-          props: allKeys,
-          computed: passthrough.computed,
-          components: passthrough.components,
+          components,
           ...(provide ? { provide } : {}),
-          render: compileTemplate(this.template)
+          render() {
+            const ctx = Object.keys(templateProps).length
+              ? merge([parent, templateProps])
+              : parent;
+            return renderFn(ctx, []);
+          }
         });
   
-        return h(dynamic, finalProps);
+        const vnode = h(dynamic);
+        if (parent.$?.appContext) {
+          vnode.appContext = parent.$.appContext;
+        }
+        return vnode;
       }
     }
   };
