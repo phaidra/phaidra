d9cb3db28a1a6806593b916fd6231b6dd5a7d86d
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-07-25 11:15:21 +0530

    vuetify component css override

--- src/phaidra-ui/plugins/vuetify.js
+++ src/phaidra-ui/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '~/assets/css/vuetify-overrides.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
