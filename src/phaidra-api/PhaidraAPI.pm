f3b616ba7008428e0855aea4c1e8db33c24f0d05
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-07-25 10:36:35 +0530

    lint fix

--- src/phaidra-api/PhaidraAPI.pm
+++ src/phaidra-api/PhaidraAPI.pm
@@ -175,12 +175,12 @@ sub startup {
   );
 
   # Override log level
-  my $privconfig = eval { $self->mongo->get_collection('config')->find_one({config_type => 'private'}) };
+  my $privconfig = eval {$self->mongo->get_collection('config')->find_one({config_type => 'private'})};
   if ($@) {
     $self->log->warn("Could not read private config for log level: $@");
   }
   elsif ($privconfig && $privconfig->{loglevel}) {
-    my %valid_levels = map { $_ => 1 } qw(trace debug info warn error fatal);
+    my %valid_levels = map {$_ => 1} qw(trace debug info warn error fatal);
     if ($valid_levels{$privconfig->{loglevel}}) {
       $self->log->level($privconfig->{loglevel});
       $self->log->info("Log level set from private config: " . $privconfig->{loglevel});
