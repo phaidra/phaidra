a78c85b9019797f7cb12233cb283276cce99338d
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-08-06 16:36:27 +0530

    nested cols and rows fix

--- src/phaidra-vue-components/src/components/display/PDDate.vue
+++ src/phaidra-vue-components/src/components/display/PDDate.vue
@@ -1,15 +1,16 @@
 <template>
   <v-row>
     <v-col :md="labelColMd" cols="12" class="pdlabel text-secondary font-weight-bold text-md-right">{{ $t(p) }}</v-col>
-    <v-col :md="valueColMd" cols="12" v-if="isEdmTimeSpan" >
-      <v-row v-for="(l, i) in o['skos:prefLabel']" :key="'prl'+i">
-        <v-col :md="valueColMd" cols="12" v-if="o['skos:exactMatch']">
-          <a class="valuefield" :href="getIDResolverURL(o['skos:exactMatch'][0])" target="_blank">{{ l['@value'] }}</a>
-        </v-col>
-        <v-col v-else :md="valueColMd" cols="12">
-          {{ l['@value'] }}
-        </v-col>
-      </v-row>
+    <v-col :md="valueColMd" cols="12" v-if="isEdmTimeSpan">
+      <template v-for="(l, i) in o['skos:prefLabel']" :key="'prl'+i">
+        <a
+          v-if="o['skos:exactMatch']"
+          class="valuefield"
+          :href="getIDResolverURL(o['skos:exactMatch'][0])"
+          target="_blank"
+        >{{ l['@value'] }}</a>
+        <span v-else>{{ l['@value'] }}</span>
+      </template>
     </v-col>
     <v-col v-else :md="valueColMd" cols="12">{{ checkAndUpdateField(o) }}</v-col>
   </v-row>
