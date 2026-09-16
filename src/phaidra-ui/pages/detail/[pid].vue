db4c01b5a4195c26e1bd86d0a96e2507be4d3581
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-09-16 12:08:45 +0530

    common page not found for detail with non existance pid and no pid, breadcrumb on detail only if pid contains

--- src/phaidra-ui/pages/detail/[pid].vue
+++ src/phaidra-ui/pages/detail/[pid].vue
@@ -1,15 +1,7 @@
 <template>
   <v-container fluid>
     <template v-if="notFound || detailPageNotFound">
-      <v-row justify="center" class="mt-8">
-        <v-col cols="12" md="8">
-          <v-alert type="error" variant="outlined" class="not-found-alert">
-            {{ $t('This page does not exist or the object cannot be found.') }}
-            <p class="mt-4">{{$t('We apologise for the inconvenience, the page you are trying to access does not exist at this address.')}}</p>
-            <p v-if="instanceconfig.email">{{$t('If you are sure you entered the correct address but still get an error, please contact')}} <a :href="'mailto:' + instanceconfig.email">{{ instanceconfig.email }}</a>.</p>
-          </v-alert>
-        </v-col>
-      </v-row>
+      <page-not-found />
     </template>
     <template v-else-if="objectInfo">
       <collection-dialog
