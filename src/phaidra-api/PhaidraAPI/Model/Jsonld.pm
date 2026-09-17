f2e80c57f1df431fcbfd16160431d2d69ab727d9
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2026-09-17 16:40:47 +0530

    Added oefos2012 to the JSON-LD namespaces

--- src/phaidra-api/PhaidraAPI/Model/Jsonld.pm
+++ src/phaidra-api/PhaidraAPI/Model/Jsonld.pm
@@ -28,6 +28,7 @@ our $namespaces = {
   foaf         => {label => "FOAF (Friend of a Friend)",                                      IRI => "http://xmlns.com/foaf/0.1/"},
   opaque       => {label => "OpaqueNamespace",                                                IRI => "http://opaquenamespace.org/"},
   oaire        => {label => "OpenAIRE Guidelines Application Profile",                        IRI => "http://namespace.openaire.eu/schema/oaire/"},
+  oefos2012    => {label => "ÖFOS 2012",                                                      IRI => "https://vocabs.acdh.oeaw.ac.at/oefosdisciplines/"},
   pcdm         => {label => "Portland Common Data Model",                                     IRI => "http://pcdm.org/models#"},
   rdau         => {label => "RDA Unconstrained",                                              IRI => "http://rdaregistry.info/Elements/u/"},
   rdf          => {label => "The RDF Concepts Vocabulary (RDF)",                              IRI => "http://www.w3.org/1999/02/22-rdf-syntax-ns#"},
